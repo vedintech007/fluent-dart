6f4c2aaf4b2da501f0e9cbfcb86f3867e9903363
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2022-11-23 22:03:30 +0000

    first commit

---
+++ bin/mastering_dart.dart
@@ -0,0 +1,5 @@
+import 'package:mastering_dart/mastering_dart.dart' as mastering_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mastering_dart.calculate()}!');
+}
---
+++ test/mastering_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:mastering_dart/mastering_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
