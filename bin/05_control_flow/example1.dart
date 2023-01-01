a2d67f2e0102d7b3ae8f080a3564f4e0e47f4aae
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-01-01 23:58:18 +0000

    explored first if else

---
+++ bin/05_control_flow/example1.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  const yourName = "Foo";
+  const myName = "Bar";
+
+  if (yourName == myName) {
+    print("We have the same name");
+  } else if (yourName == "Foo") {
+    print("your name is foo");
+  } else {
+    print("Something else");
+  }
+}
