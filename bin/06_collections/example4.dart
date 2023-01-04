ed1bd1a8e5ea2537d06999946a8716f7edb4c860
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-01-04 17:37:56 +0000

    maps

--- bin/06_collections/example4.dart
+++ bin/06_collections/example4.dart
@@ -3,4 +3,33 @@ void main(List<String> args) {
     'name': "Foo",
     'age': 10,
   };
+
+  // print(info);
+  // print(info['name']);
+  // print(info['age']);
+
+  // print(info.keys);
+  // print(info.values);
+
+  // info.putIfAbsent("girlFriend", () => "None");
+  // info['girlFriend'] = "Salomey";
+
+  // print(info);
+
+  // for (final entry in info.entries) {
+  //   print("Key is ${entry.key} and value is ${entry.value}");
+  // }
+
+  // if (info.containsKey('name')) {
+  //   print("Yes");
+  // } else {
+  //   print("Nope");
+  // }
+
+  final mapWithIntKeys = {
+    10: 20,
+    20: 40,
+  };
+
+  print(mapWithIntKeys[12]);
 }
