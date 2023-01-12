0b1768671ffdbc6c4d5393430b920319a5f1de56
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-01-12 21:01:01 +0000

    done with classes

--- bin/08_class/example10.dart
+++ bin/08_class/example10.dart
@@ -1,1 +1,42 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final car = Car();
+
+  print(car);
+  car.accelerate();
+  car.decelerate();
+
+  final mc = Motocycle();
+
+  print(mc);
+}
+
+abstract class Vehicle {
+  final Vehiclekind kind;
+
+  const Vehicle({required this.kind});
+
+  void accelerate() => print("${kind.name} is accelerating");
+  void decelerate() => print("${kind.name} is decelerating");
+}
+
+class Car extends Vehicle {
+  Car() : super(kind: Vehiclekind.car);
+}
+
+class Motocycle implements Vehicle {
+  @override
+  void accelerate() => print("Motocycle is accelerating");
+
+  @override
+  void decelerate() => print("Motocycle is decelerating");
+
+  @override
+  Vehiclekind get kind => Vehiclekind.motorcycle;
+}
+
+enum Vehiclekind {
+  car,
+  truck,
+  motorcycle,
+  bicycle,
+}
