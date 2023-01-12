c15b68836638180d833da8fe7947e8d30a3b5d0b
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-01-12 16:17:32 +0000

    inheriting contructors

--- bin/08_class/example9.dart
+++ bin/08_class/example9.dart
@@ -1,1 +1,23 @@
 void main(List<String> args) {}
+
+enum Role {
+  mum,
+  dad,
+  sis,
+  bro,
+  gma,
+}
+
+class Person {
+  final Role role;
+
+  const Person({required this.role});
+}
+
+class Mom extends Person {
+  const Mom() : super(role: Role.mum);
+}
+
+class Dad extends Person {
+  const Dad() : super(role: Role.dad);
+}
